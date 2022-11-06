ecdeace072faeee8e4ad5d4295586a2ca0a55261
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-06 15:45:22 +0200

    Add basic web

---
+++ C# Fundamentals/10. Basic Web/ParkingSystem/Controllers/CarController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ParkingSystem.Data;
+using ParkingSystem.Data.Models;
+using System.Linq;
+
+namespace ParkingSystem.Controllers
+{
+    public class CarController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            DataAccess.Cars.Add(car);
+            return Redirect("/");
+        }
+        [HttpPost]
+        public IActionResult DeleteCar(string plateNumber)
+        {
+            Car car = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);
+            DataAccess.Cars.Remove(car);
+            return Redirect("/");
+        }
+    }
+}
