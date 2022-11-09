f8249a35f84a276c01742b68b34dd35069395897
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-09 11:19:02 +0200

    add exercise

--- C# Advanced/01. Stacks and Queues/Exercise/12. Cups and Bottles/Program.cs
+++ C# Advanced/01. Stacks and Queues/Exercise/12. Cups and Bottles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _12._Cups_and_Bottles
 {
@@ -6,7 +8,45 @@ namespace _12._Cups_and_Bottles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> cupsCapacity = new List<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> bottle = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int wasteWater = 0;
+            while (cupsCapacity.Any() && bottle.Any())
+            {
+                int currentBottle = bottle.Pop();
+                if (currentBottle >= cupsCapacity.First())
+                {
+                    wasteWater += currentBottle - cupsCapacity.First();
+                    cupsCapacity[0] -= currentBottle;
+                }
+                else
+                {
+                    while (cupsCapacity[0] > 0 && bottle.Any())
+                    {
+                        currentBottle += bottle.Pop();
+                        if (currentBottle >= cupsCapacity.First())
+                        {
+                            wasteWater += currentBottle - cupsCapacity.First();
+                            cupsCapacity[0] -= currentBottle;
+                        }
+                    }
+                }
+                if (cupsCapacity[0] <= 0)
+                {
+                    cupsCapacity.RemoveAt(0);
+                }
+            }
+
+            if (bottle.Any())
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottle)}");
+            }
+            else if (cupsCapacity.Any())
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cupsCapacity)}");
+            }
+            Console.WriteLine($"Wasted litters of water: {wasteWater}");
         }
+
     }
 }
