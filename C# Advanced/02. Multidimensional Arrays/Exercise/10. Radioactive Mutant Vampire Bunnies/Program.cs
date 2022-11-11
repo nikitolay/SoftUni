cda5137fd825237b2876d30519c801ebdc00d450
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-12 00:30:40 +0200

    add exercise

---
+++ C# Advanced/02. Multidimensional Arrays/Exercise/10. Radioactive Mutant Vampire Bunnies/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._Radioactive_Mutant_Vampire_Bunnies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int row = sizes[0];
+            int col = sizes[1];
+            char[,] matrix = new char[row, col];
+            int[] array = FillMatrix(matrix);
+            int playerRow = array[0];
+            int playerCol = array[1];
+
+            string coordinates = Console.ReadLine();
+            bool win = false;
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                char coordinat = coordinates[i];
+
+                matrix[playerRow, playerCol] = '.';
+                if (coordinat == 'U')
+                {
+                    if (!IsInside(matrix, playerRow - 1, playerCol))
+                    {
+                        win = true;
+                    }
+                    else
+                    {
+
+                        matrix[--playerRow, playerCol] = 'P';
+                    }
+
+
+                }
+                else if (coordinat == 'D')
+                {
+
+                    if (!IsInside(matrix, playerRow + 1, playerCol))
+                    {
+                        win = true;
+                    }
+                    else
+                    {
+
+                        matrix[++playerRow, playerCol] = 'P';
+                    }
+
+                }
+                else if (coordinat == 'L')
+                {
+
+                    if (!IsInside(matrix, playerRow, playerCol - 1))
+                    {
+                        win = true;
+                    }
+                    else
+                    {
+
+                        matrix[playerRow, --playerCol] = 'P';
+                    }
+
+                }
+                else if (coordinat == 'R')
+                {
+
+                    if (!IsInside(matrix, playerRow, playerCol + 1))
+                    {
+                        win = true;
+                    }
+                    else
+                    {
+
+                        matrix[playerRow, ++playerCol] = 'P';
+                    }
+
+                }
+                SetBunnies(matrix);
+                if (matrix[playerRow, playerCol] == 'B')
+                {
+                    PrintMatrix(matrix);
+                    Console.WriteLine($"dead: {playerRow} {playerCol}");
+                    return;
+                }
+                if (win)
+                {
+                    PrintMatrix(matrix);
+                    Console.WriteLine($"won: {playerRow} {playerCol}");
+                    return;
+                }
+
+
+
+            }
+        }
+
+        private static void SetBunnies(char[,] matrix)
+        {
+            List<int> bunnies = GetBunnies(matrix);
+            while (bunnies.Any())
+            {
+                int bunnyRow = bunnies[0];
+                int bunnyCol = bunnies[1];
+
+                if (IsInside(matrix, bunnyRow, bunnyCol - 1))
+                {
+                    matrix[bunnyRow, bunnyCol - 1] = 'B';
+                }
+                if (IsInside(matrix, bunnyRow, bunnyCol + 1))
+                {
+                    matrix[bunnyRow, bunnyCol + 1] = 'B';
+                }
+                if (IsInside(matrix, bunnyRow - 1, bunnyCol))
+                {
+                    matrix[bunnyRow - 1, bunnyCol] = 'B';
+                }
+                if (IsInside(matrix, bunnyRow + 1, bunnyCol))
+                {
+                    matrix[bunnyRow + 1, bunnyCol] = 'B';
+                }
+                bunnies.RemoveAt(0);
+                bunnies.RemoveAt(0);
+            }
+
+        }
+
+        private static List<int> GetBunnies(char[,] matrix)
+        {
+            List<int> coordinates = new List<int>();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 'B')
+                    {
+                        coordinates.Add(i);
+                        coordinates.Add(j);
+                    }
+
+                }
+            }
+            return coordinates;
+        }
+
+        private static bool IsInside(char[,] matrix, int v, int playerCol)
+        {
+            return v >= 0 && playerCol >= 0 && v < matrix.GetLength(0) && playerCol < matrix.GetLength(1);
+        }
+
+        private static bool IsBunny(char[,] matrix, int v, int playerCol)
+        {
+            return matrix[v, playerCol] == 'B' && IsInside(matrix, v, playerCol);
+        }
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        private static int[] FillMatrix(char[,] matrix)
+        {
+            int[] arr = new int[matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string characters = Console.ReadLine();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = characters[j];
+                    if (matrix[i, j] == 'P')
+                    {
+
+                        arr[0] = i;
+                        arr[1] = j;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
---
+++ C# Advanced/02. Multidimensional Arrays/Exercise/9. Miner/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _9._Miner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            List<string> commands = Console.ReadLine().Split(' ').ToList();
+            int minerRow = 0;
+            int minerCol = 0;
+            int coals = 0;
+            for (int i = 0; i < size; i++)
+            {
+                char[] characters = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = characters[j];
+                    if (matrix[i, j] == 's')
+                    {
+                        minerRow = i;
+                        minerCol = j;
+                    }
+                    if (matrix[i, j] == 'c')
+                    {
+                        coals++;
+                    }
+                }
+            }
+            int currentCoal = 0;
+            bool isFinished = false;
+            while (commands.Any())
+            {
+                char[] currentResult = MoveIt(matrix, commands[0], ref minerRow, ref minerCol);
+                if (commands[0] == "left")
+                {
+
+                }
+                if (currentResult[0] == 'e')
+                {
+                    Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+                    isFinished = true;
+                    break;
+                }
+                else if (currentResult[0] == 'c')
+                {
+
+                    currentCoal++;
+
+                }
+                if (currentCoal == coals)
+                {
+                    Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+                    isFinished = true;
+                    break;
+                }
+
+
+
+
+                commands.RemoveAt(0);
+            }
+            if (!isFinished)
+            {
+                Console.WriteLine($"{coals - currentCoal} coals left. ({minerRow}, {minerCol})");
+            }
+
+        }
+
+
+        private static char[] MoveIt(char[,] matrix, string v, ref int minerRow, ref int minerCol)
+        {
+            char[] result = new char[matrix.GetLength(0)];
+            if (v == "left" && IsInside(matrix, minerRow, minerCol - 1))
+            {
+                if (matrix[minerRow, minerCol - 1] == 'e')
+                {
+                    result[0] = 'e';
+                }
+                else
+                {
+                    if (matrix[minerRow, minerCol - 1] == 'c')
+                    {
+                        result[0] = 'c';
+                    }
+                    matrix[minerRow, minerCol] = '*';
+                    matrix[minerRow, minerCol - 1] = 's';
+                }
+                minerCol--;
+
+            }
+            else if (v == "right" && IsInside(matrix, minerRow, minerCol + 1))
+            {
+                if (matrix[minerRow, minerCol + 1] == 'e')
+                {
+                    result[0] = 'e';
+                }
+                else
+                {
+                    if (matrix[minerRow, minerCol + 1] == 'c')
+                    {
+                        result[0] = 'c';
+                    }
+
+                    matrix[minerRow, minerCol] = '*';
+                    matrix[minerRow, minerCol + 1] = 's';
+                }
+                minerCol++;
+
+            }
+            else if (v == "up" && IsInside(matrix, minerRow - 1, minerCol))
+            {
+                if (matrix[minerRow - 1, minerCol] == 'e')
+                {
+                    result[0] = 'e'; ;
+                }
+                else
+                {
+                    if (matrix[minerRow - 1, minerCol] == 'c')
+                    {
+                        result[0] = 'c'; ;
+                    }
+                    matrix[minerRow, minerCol] = '*';
+                    matrix[minerRow - 1, minerCol] = 's';
+                }
+                minerRow--;
+
+
+            }
+            else if (v == "down" && IsInside(matrix, minerRow, minerCol))
+            {
+                if (matrix[minerRow + 1, minerCol] == 'e')
+                {
+                    result[0] = 'e';
+                }
+                else
+                {
+                    if (matrix[minerRow + 1, minerCol] == 'c')
+                    {
+                        result[0] = 'c';
+                    }
+                    matrix[minerRow, minerCol] = '*';
+                    matrix[minerRow + 1, minerCol] = 's';
+                }
+                minerRow++;
+
+            }
+            return result;
+        }
+
+        private static bool IsInside(char[,] matrix, int minerRow, int minerCol)
+        {
+            return minerRow < matrix.GetLength(0) && minerRow >= 0 && minerCol < matrix.GetLength(1) && minerCol >= 0;
+        }
+    }
+}
