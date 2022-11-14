fb52ae09a26d8d6c3f3e1a8e5bd218f92c11e5f0
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-14 21:33:56 +0200

    add exercise

---
+++ C# Advanced/06. Defining classes/Exercise/10.SoftUniParking/Parking.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    class Parking
+    {
+        public Parking(int capacity)
+        {
+            Cars = new List<Car>();
+            Capacity = capacity;
+        }
+        public List<Car> Cars { get; set; }
+        public int Count  => Cars.Count();
+        public int Capacity { get; set; }
+
+        public string AddCar(Car car)
+        {
+            if (Cars.Contains(car))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else if (Cars.Count > Capacity)
+            {
+                return "Parking is full!";
+            }
+            else
+            {
+                Cars.Add(car);
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+        }
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!Cars.Any(x => x.RegistrationNumber == registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                Car currentCar = Cars.FirstOrDefault(x => x.RegistrationNumber == registrationNumber);
+                Cars.Remove(currentCar);
+                return $"Successfully removed {registrationNumber}";
+            }
+        }
+        public Car GetCar(string registrationNumber)
+        {
+
+            return Cars.FirstOrDefault(x => x.RegistrationNumber == registrationNumber);
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+
+        }
+    }
+}
---
+++ C# Advanced/06. Defining classes/Exercise/SoftUniParking/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+        public override string ToString()
+        {
+
+            //Make: Skoda 
+
+            //Model: Fabia 
+
+            //HorsePower: 65 
+
+            //RegistrationNumber: CC1856BG 
+            return @$"Make: {Make}
+Model: {Model} 
+HorsePower: {HorsePower} 
+RegistrationNumber: {RegistrationNumber} ";
+        }
+    }
+}
