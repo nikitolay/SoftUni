2197bcc65726256f8b501f9a5caaa3674c1cfa30
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-16 11:34:58 +0200

    add lab

---
+++ C# Advanced/08. Generics/Lab/BoxOfT/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        Stack<T> items;
+        public Box()
+        {
+            items = new Stack<T>();
+        }
+        public int Count => items.Count;
+        public void Add(T item)
+        {
+            items.Push(item);
+        }
+        public T Remove()
+        {
+            if (Count >= 1)
+            {
+               return items.Pop();
+            }
+            return default;
+        }
+    }
+}
