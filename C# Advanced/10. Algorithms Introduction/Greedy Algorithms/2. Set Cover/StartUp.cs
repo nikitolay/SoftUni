862340b4a9db7d8e0d386ccc9745f73db49f83e6
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-18 12:08:07 +0200

    complete greedy algorithm

---
+++ C# Advanced/10. Algorithms Introduction/Greedy Algorithms/2. Set Cover/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace _2._Set_Cover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> universe = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine());
+            List<int[]> sets = new List<int[]>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] set = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                sets.Add(set);
+            }
+            List<int[]> result = ChooseSets(sets, universe);
+            Console.WriteLine($"Sets to take ({result.Count()}):");
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{{ {string.Join(", ", item)} }}");
+            }
+        }
+
+        public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+        {
+
+            List<int[]> result = new List<int[]>();
+            while (sets.Count > 0 && universe.Count > 0)
+            {
+                int[] current = sets.OrderByDescending(x => x.Count(x => universe.Contains(x))).FirstOrDefault();
+                foreach (var item in current)
+                {
+                    if (universe.Contains(item))
+                    {
+                        universe.Remove(item);
+                    }
+                }
+                result.Add(current);
+            }
+
+            return result;
+        }
+    }
+}
