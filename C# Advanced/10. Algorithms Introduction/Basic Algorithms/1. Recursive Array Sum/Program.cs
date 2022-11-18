7301830e4a0bea07b32d66da271e6eca56a7ad7d
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-18 11:16:20 +0200

    add algorithms

---
+++ C# Advanced/10. Algorithms Introduction/Basic Algorithms/1. Recursive Array Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(arr, 0));
+        }
+        static int Sum(int[] arr, int index)
+        {
+            if (arr.Length == index)
+            {
+                return 0;
+            }
+            return arr[index] + Sum(arr, index + 1);
+        }
+    }
+}
---
+++ C# Advanced/10. Algorithms Introduction/Basic Algorithms/2. Recursive Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        static long Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
