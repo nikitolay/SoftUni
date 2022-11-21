9b4d77953f8e0df5fb16b855855cbd0d86473daa
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-21 12:14:15 +0200

    add lab

---
+++ C# OOP/Lab/4. RandomList/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private List<string> list;
+        private Random random;
+        public RandomList()
+        {
+            list = new List<string>();
+            random = new Random();
+        }
+        public string RandomString()
+        {
+            int indexOfRemove = random.Next(0, list.Count);
+            string textToRemove = list[indexOfRemove];
+            list.RemoveAt(indexOfRemove);
+            return textToRemove;
+
+        }
+    }
+}
---
+++ C# OOP/Lab/5. StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        private Stack<string> stack;
+        public StackOfStrings()
+        {
+            stack = new Stack<string>();
+        }
+        public bool IsEmpty()
+        {
+            return stack.Count == 0;
+        }
+        public void AddRange(Stack<string> stack)
+        {
+            while (stack.Count > 0)
+            {
+                this.stack.Push(stack.Pop());
+            }
+        }
+    }
+}
