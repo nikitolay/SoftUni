48c02e8f84a478f3e8a2481340c560c34f75da50
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-23 11:27:29 +0200

    add lab

---
+++ C# OOP/03. Interfaces and Abstraction/Lab/Cars/Seat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get ; set; }
+        public string Color { get ; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Seat {this.Model}");
+            sb.AppendLine(this.Start());
+            sb.Append(this.Stop());
+
+            return sb.ToString();
+        }
+    }
+}
