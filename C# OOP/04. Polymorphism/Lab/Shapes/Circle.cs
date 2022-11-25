09988269d26158e4888dc2569b8e03c090d4cf1d
Nikolay Pavlov <npavlovit@gmail.com>
2022-11-25 12:28:53 +0200

    add lab

---
+++ C# OOP/04. Polymorphism/Lab/Shapes/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Math.PI * (2 * radius);
+        }
+        public override string Draw()
+        {
+            return base.Draw() + "Circle";
+        }
+    }
+}
