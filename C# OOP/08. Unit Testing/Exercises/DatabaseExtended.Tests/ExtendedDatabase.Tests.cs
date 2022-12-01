43bb08035dba58c2a077c87120c25aad98e486af
Nikolay Pavlov <npavlovit@gmail.com>
2022-12-01 23:33:46 +0200

    add exercises

--- C# OOP/08. Unit Testing/Exercises/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
+++ C# OOP/08. Unit Testing/Exercises/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
@@ -1,18 +1,58 @@
+using ExtendedDatabase;
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
     public class ExtendedDatabaseTests
     {
+        private Person person;
+        private ExtendedDatabase.ExtendedDatabase database;
+
         [SetUp]
         public void Setup()
         {
+            database = new ExtendedDatabase.ExtendedDatabase();
+            person = new Person(11, "Nikolay");
         }
 
         [Test]
-        public void Test1()
+        public void AddMethodShouldThrowExceptionWhenAlreadyExistPersonWithSameUsername()
         {
-            Assert.Pass();
+            database.Add(person);
+            Person secondPerson = new Person(101, "Nikolay");
+
+            Assert.Throws<InvalidOperationException>(() => database.Add(secondPerson));
         }
+
+        [Test]
+        public void AddMethodShouldThrowExceptionWhenAlreadyExistPersonWithSameId()
+        {
+            database.Add(person);
+            Person secondPerson = new Person(11, "Ivan");
+            Assert.Throws<InvalidOperationException>(() => database.Add(secondPerson));
+
+        }
+
+        [Test]
+        public void RemoveMethodShouldThrowExceptionWhenThereAreNoPeople()
+        {
+            Assert.Throws<InvalidOperationException>(() => database.Remove());
+        }
+
+        [Test]
+        public void RemoveMethodShouldDecreaseCountOfPeople()
+        {
+            database.Add(person);
+            for (int i = 0; i < 3; i++)
+            {
+                database.Add(new Person(i, i + ""));
+            }
+            database.Remove();
+            Assert.AreEqual(3, database.Count);
+        }
+
+
+
     }
 }
