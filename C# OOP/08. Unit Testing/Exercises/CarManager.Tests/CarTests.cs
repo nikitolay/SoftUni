426ce61d20cc27e8aba808e87a3b469d7e83f885
Nikolay Pavlov <npavlovit@gmail.com>
2022-12-02 12:28:22 +0200

    add unit tests

--- C# OOP/08. Unit Testing/Exercises/CarManager.Tests/CarTests.cs
+++ C# OOP/08. Unit Testing/Exercises/CarManager.Tests/CarTests.cs
@@ -1,18 +1,153 @@
+using CarManager;
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
     public class CarTests
     {
+        Car car;
         [SetUp]
         public void Setup()
         {
+            car = new Car("Mercedes", "Maybach", 16.8, 200);
         }
 
         [Test]
-        public void Test1()
+        public void ConstructorSetValueOnCarMake()
         {
-            Assert.Pass();
+
+            Assert.AreEqual("Mercedes", car.Make);
+
+        }
+        [Test]
+        public void ConstructorSetValueOnCarModel()
+        {
+
+            Assert.AreEqual("Maybach", car.Model);
+        }
+        [Test]
+        public void ConstructorSetValueOnCarFuelConsumption()
+        {
+
+            Assert.AreEqual(16.8, car.FuelConsumption);
+        }
+        [Test]
+        public void ConstructorSetValueOnCarFuelCapacity()
+        {
+
+            Assert.AreEqual(200, car.FuelCapacity);
+        }
+        [Test]
+        public void ConstructorSetDefaultValueOnCarFuelAmount()
+        {
+
+            Assert.AreEqual(0, car.FuelAmount);
+        }
+        [Test]
+        public void MakePropertyShouldReturnTheMake()
+        {
+
+            Assert.AreEqual("Mercedes", car.Make);
+        }
+
+        [Test]
+        public void MakePropertyShouldThrowExceptionForNullOrEmptyString()
+        {
+            Assert.Throws<ArgumentException>(() => new Car("", "Maybach", 16.8, 200));
+        }
+
+
+        [Test]
+        public void ModelPropertyShouldReturnTheModel()
+        {
+
+            Assert.AreEqual("Maybach", car.Model);
+        }
+
+        [Test]
+        public void ModelPropertyShouldThrowExceptionForNullOrEmptyString()
+        {
+            Assert.Throws<ArgumentException>(() => new Car("Mercedes", "", 16.8, 200));
+        }
+
+
+        [Test]
+        public void FuelConsumptionPropertyShouldReturnTheFuelConsumption()
+        {
+
+            Assert.AreEqual(16.8, car.FuelConsumption);
+        }
+
+        [Test]
+        public void FuelConsumptionPropertyShouldThrowExceptionForNegativeFuelConsumption()
+        {
+            Assert.Throws<ArgumentException>(() => new Car("Mercedes", "Maybach", -16.8, 200));
+        }
+
+
+        [Test]
+        public void FuelAmountPropertyShouldReturnTheFuelAmount()
+        {
+            car.Refuel(200);
+            Assert.AreEqual(200, car.FuelAmount);
+        }
+
+        [Test]
+        public void FuelAmountPropertyShouldThrowExceptionForNegativeFuelAmount()
+        {
+            Assert.Throws<ArgumentException>(() => car.Refuel(-500));
+        }
+
+        [Test]
+        public void FuelCapacityPropertyShouldReturnTheFuelCapacity()
+        {
+            Assert.AreEqual(200, car.FuelCapacity);
+        }
+
+        [Test]
+        public void FuelCapacityPropertyShouldThrowExceptionForNegativeFuelCapacity()
+        {
+            Assert.Throws<ArgumentException>(() => new Car("Mercedes", "Maybach", 16.8, -200));
+        }
+
+
+
+        [Test]
+        public void RefuelMethodShouldThrowExceptionForNegativeFuel()
+        {
+            Assert.Throws<ArgumentException>(() => car.Refuel(-100));
+        }
+
+        [Test]
+        public void RefuelMethodShouldAddFuelToFuelAmount()
+        {
+            car.Refuel(10);
+            Assert.AreEqual(10, car.FuelAmount);
+        }
+        [Test]
+        public void RefuelMethodShouldSetValueOfFuelCapacityOnFuelAmountIfAmountIsGreaterThanCapacity()
+        {
+            car.Refuel(210);
+            Assert.AreEqual(200, car.FuelAmount);
+        }
+
+
+        [Test]
+        public void DriveMethodShouldThrowExceptionIfFuelNeededIsGreaterThanFuelAmount()
+        {
+            car.Refuel(99);
+
+            Assert.Throws<InvalidOperationException>(() => car.Drive(2000));
+        }
+
+        [Test]
+        public void DriveMethodShouldDecreaseFuelAmountWithFuelNeeded()
+        {
+            car.Refuel(100);
+            car.Drive(99);
+
+            Assert.AreEqual(83.367999999999995d, car.FuelAmount);
         }
     }
 }
--- C# OOP/08. Unit Testing/Exercises/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
+++ C# OOP/08. Unit Testing/Exercises/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
@@ -1,6 +1,7 @@
 using ExtendedDatabase;
 using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace Tests
 {
@@ -52,7 +53,71 @@ namespace Tests
             Assert.AreEqual(3, database.Count);
         }
 
+        [Test]
+        public void FindByUsernameMethodShouldReturnTheWantedPerson()
+        {
+            database.Add(person);
+            Assert.AreEqual(person, database.FindByUsername(person.UserName));
+        }
+
+        [Test]
+        public void FindByUsernameMethodShouldThrowExceptionWhenReceivedNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => database.FindByUsername(null));
+        }
+        [Test]
+        public void FindByUsernameMethodShouldThrowExceptionWhenReceivedEmptyText()
+        {
+            Assert.Throws<ArgumentNullException>(() => database.FindByUsername(string.Empty));
+        }
+
+        [Test]
+        public void FindByUsernameMethodShouldThrowExceptionWhenAGivenUserNotContainedInDatabase()
+        {
 
 
+            Person personTwo = new Person(15, "Ivan");
+
+            Assert.Throws<InvalidOperationException>(() => database.FindByUsername(personTwo.UserName));
+        }
+
+        [Test]
+        public void FindByUsernameMethodShouldThrowExceptionWhenTheCaseHasBeenReplaced()
+        {
+
+            database.Add(person);
+            Assert.Throws<InvalidOperationException>(() => database.FindByUsername("nikolay"));
+        }
+
+        [Test]
+        public void FindByIdMethodShouldReturnTheWantedPerson()
+        {
+            database.Add(person);
+            Assert.AreEqual(person, database.FindById(person.Id));
+        }
+
+        [Test]
+        public void FindByIdMethodShouldThrowExceptionWhenNotContainedUserWithSameId()
+        {
+
+            Assert.Throws<InvalidOperationException>(() => database.FindById(11));
+        }
+        [Test]
+        public void FindByIdMethodShouldThrowExceptionWhenANegativeNumberIsEntered()
+        {
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => database.FindById(-200));
+        }
+        [Test]
+        public void ConstructorShouldAddAllElementsInTheArray()
+        {
+            Person[] persons=new Person[3];
+            for (int i = 0; i < 3; i++)
+            {
+                persons[i] = new Person(i,$"{i}");
+            }
+            database = new ExtendedDatabase.ExtendedDatabase(persons);
+            Assert.AreEqual(persons.Length, database.Count);
+        }
     }
 }
