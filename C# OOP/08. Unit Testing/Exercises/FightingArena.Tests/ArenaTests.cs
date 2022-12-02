e90302452139589d73f9ed6e7a432de145bb7e7d
Nikolay Pavlov <npavlovit@gmail.com>
2022-12-03 00:45:57 +0200

    complete unit test exercises

--- C# OOP/08. Unit Testing/Exercises/FightingArena.Tests/ArenaTests.cs
+++ C# OOP/08. Unit Testing/Exercises/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,80 @@
-﻿using NUnit.Framework;
+﻿using FightingArena;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tests
 {
     public class ArenaTests
     {
+        Arena arena;
+        Warrior warrior;
         [SetUp]
         public void Setup()
         {
+            arena = new Arena();
+            warrior = new Warrior("Name", 50, 100);
         }
 
         [Test]
-        public void Test1()
+        public void EnrollMethodShouldThrowExceptionWhenWarriorIsAlreadyEnrolledForTheFights()
         {
-            Assert.Pass();
+            arena.Enroll(warrior);
+            Assert.Throws<InvalidOperationException>(() => arena.Enroll(warrior));
+        }
+        [Test]
+        public void EnrollMethodShouldAddWarrior()
+        {
+            arena.Enroll(warrior);
+            Assert.IsTrue(arena.Warriors.Any(x => x.Name == warrior.Name));
+        }
+
+        [Test]
+        public void FightMethodShouldThrowExceptionWhenТhеAttackerByNameDoesNotExist()
+        {
+            arena.Enroll(warrior);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Name", "second"));
+        }
+        [Test]
+        public void FightMethodShouldThrowExceptionWhenТhеDefenderByNameDoesNotExist()
+        {
+            arena.Enroll(warrior);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("first", "Name"));
+        }
+
+
+        [Test]
+        public void CountPropertyShouldReturnWarriorsCount()
+        {
+            arena.Enroll(warrior);
+            for (int i = 0; i < 3; i++)
+            {
+                arena.Enroll(new Warrior($"{i}", i + 20, i + 30));
+            }
+            Assert.AreEqual(4, arena.Count);
+        }
+
+        [Test]
+        public void WarriorsPropertyShouldReturnAllWarriors()
+        {
+            List<Warrior> listWithWarriors = new List<Warrior>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Warrior currWarior = new Warrior($"{i}", i + 20, i + 40);
+                arena.Enroll(currWarior);
+                listWithWarriors.Add(currWarior);
+            }
+            CollectionAssert.AreEqual(listWithWarriors, arena.Warriors);
+        }
+
+        [Test]
+        public void ConstructorShouldInitializeCollectionWithWarriors()
+        {
+            Assert.NotNull(arena.Warriors);
         }
     }
 }
--- C# OOP/08. Unit Testing/Exercises/FightingArena.Tests/WarriorTests.cs
+++ C# OOP/08. Unit Testing/Exercises/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,150 @@
+﻿using FightingArena;
 using NUnit.Framework;
+using System;
+using System.ComponentModel;
 
 namespace Tests
 {
     public class WarriorTests
     {
+        Warrior warrior;
         [SetUp]
         public void Setup()
         {
+            warrior = new Warrior("Ivan", 50, 100);
         }
 
         [Test]
-        public void Test1()
+        public void NamePropertyShouldThrowExceptionWhenValueIsNull()
         {
-            Assert.Pass();
+            Assert.Throws<ArgumentException>(() => new Warrior(null, 50, 100));
+        }
+
+
+        [Test]
+        public void NamePropertyShouldThrowExceptionWhenValueIsWhiteSpace()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("", 50, 100));
+        }
+
+        [Test]
+        public void NamePropertyShouldSetValueOnName()
+        {
+
+            Assert.AreEqual("Ivan", warrior.Name);
+        }
+        [Test]
+        public void NamePropertyShouldReturnValueOfName()
+        {
+
+            Assert.AreEqual("Ivan", warrior.Name);
+        }
+
+        [Test]
+        public void DamagePropertyShouldThrowExceptionWhenGivenDamageIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Ivan", -50, 100));
+        }
+        [Test]
+        public void DamagePropertyShouldSetValueOnDamage()
+        {
+            Assert.AreEqual(50, warrior.Damage);
+        }
+        [Test]
+        public void DamagePropertyShouldReturnValueOfDamage()
+        {
+            Assert.AreEqual(50, warrior.Damage);
+        }
+
+        [Test]
+        public void HpPropertyShouldThrowExceptionWhenGivenHpIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Ivan", 50, -100));
+        }
+
+        [Test]
+        public void HpPropertyShouldSetValueOnHp()
+        {
+            Assert.AreEqual(100, warrior.HP);
+        }
+
+        [Test]
+        public void HpPropertyShouldReturnValueOfHp()
+        {
+            Assert.AreEqual(100, warrior.HP);
+        }
+
+        [Test]
+        public void AttackMethodShouldThrowExceptionWhenHpIsLessThanToMinHp()
+        {
+            warrior = new Warrior("Ivan", 30, 29);
+
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(warrior));
+        }
+        [Test]
+        public void AttackMethodShouldThrowExceptionWhenHpIsEqualToMinHp()
+        {
+            warrior = new Warrior("Ivan", 30, 30);
+
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(warrior));
+        }
+
+        [Test]
+        public void AttackMethodShouldThrowExceptionWhenHpOfOpponentIsLessThanToMinHp()
+        {
+
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(new Warrior("Ivan", 30, 29)));
+        }
+        [Test]
+        public void AttackMethodShouldThrowExceptionWhenHpOfOpponentIsLEqualToMinHp()
+        {
+
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(new Warrior("Ivan", 30, 30)));
+        }
+
+        [Test]
+        public void AttackMethodShouldThrowExceptionWhenHpIsLessThanToOpponentDamage()
+        {
+
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(new Warrior("Ivan", 101, 29)));
+        }
+
+        [Test]
+        public void AttackMethodShouldDecreaseHpWithTheOpponentDamage()
+        {
+            Warrior opponent = new Warrior("name", 20, 100);
+            warrior.Attack(opponent);
+            Assert.AreEqual(80, warrior.HP);
+        }
+
+        [Test]
+        public void AttackMethodShouldSetNullOnOpponentHpWhenAttackingDamageIsGreaterThanToOpponentHp()
+        {
+            Warrior opponent = new Warrior("name", 20, 40);
+            warrior.Attack(opponent);
+            Assert.AreEqual(0, opponent.HP);
+        }
+        [Test]
+        public void AttackMethodShouldDecreaseOpponentHpWithAttackingDamage()
+        {
+            Warrior opponent = new Warrior("name", 20, 60);
+            warrior.Attack(opponent);
+            Assert.AreEqual(10, opponent.HP);
+        }
+        [Test]
+        public void ConstructorShouldSetValueOfHp()
+        {
+            Assert.AreEqual(100, warrior.HP);
+        }
+        [Test]
+        public void ConstructorShouldSetValueOfDamage()
+        {
+            Assert.AreEqual(50, warrior.Damage);
+        }
+        [Test]
+        public void ConstructorShouldSetValueOfName()
+        {
+            Assert.AreEqual("Ivan", warrior.Name);
         }
     }
 }
