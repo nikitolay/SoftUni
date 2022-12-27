9b347f1301c75faf5dadabd9cea8c5f166b8cba8
Nikolay Pavlov <npavlovit@gmail.com>
2022-12-27 17:56:10 +0200

    add studentSystem

---
+++ Entity Framework Core/04. Entity Relations/StudentSystem/P01_StudentSystem/Data/Models/Homework.cs
@@ -0,0 +1,33 @@
+﻿using P01_StudentSystem.Data.Models.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        [Key]
+        public int HomeworkId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(max)")]
+        public string Content { get; set; }
+
+        public ContentType ContentType { get; set; }
+
+        public DateTime SubmissionTime { get; set; }
+
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
---
+++ Entity Framework Core/04. Entity Relations/StudentSystem/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> Homework { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost;Database=StudentSystem;User ID=sa;Password=PavlovNik4312;TrustServerCertificate=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(x => new { x.StudentId, x.CourseId });
+        }
+    }
+}
