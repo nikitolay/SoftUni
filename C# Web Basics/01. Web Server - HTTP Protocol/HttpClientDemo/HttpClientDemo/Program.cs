cea641398a3d2057935695bb2cef89546051d9f1
Nikolay Pavlov <npavlovit@gmail.com>
2023-01-19 20:40:15 +0200

    Changes after Async lecture

--- C# Web Basics/01. Web Server - HTTP Protocol/HttpClientDemo/HttpClientDemo/Program.cs
+++ C# Web Basics/01. Web Server - HTTP Protocol/HttpClientDemo/HttpClientDemo/Program.cs
@@ -7,47 +7,74 @@ namespace HttpClientDemo
 {
     internal class Program
     {
+        const string NewLine = "\r\n";
         static async Task Main(string[] args)
         {
-            const string NewLine = "\r\n";
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);//loopack==localhost,open port 80
             tcpListener.Start();//give me this port 80 and start work  
 
             while (true)
             {
                 var client = tcpListener.AcceptTcpClient();//the readline on tcp//read client
-                using var stream = client.GetStream();//to the client take a stream   
-
-                int byteLenght = 0;
-                byte[] buffer = new byte[100000];
-                var lenght = stream.Read(buffer, byteLenght, buffer.Length);
-
-                string requestString = Encoding.UTF8.GetString(buffer, 0, lenght); //convert what the client wrote you into a byte array
-
-                Console.WriteLine(requestString);
-
-                Thread.Sleep(5000);
-
-                string html = $"<h1> Hello from NikiServer {DateTime.Now}</h1>" +
-                    $"<form method=post><input name=username /><input name=password />" +
-                    $"<input type=submit /></form>"+DateTime.Now;
-
-                string response = "HTTP/1.1 200 OK" + NewLine +
-                    "Server: NikiServer 2022" + NewLine +
-                    // "Location: https://google.com" + NewLine +
-                    "Content-Type: text/html; charset=utf-8" + NewLine +
-                    "Content-Lenght: " + html.Length + NewLine +
-                    NewLine +
-                    html +
-                    NewLine;
-
-                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                stream.Write(responseBytes);
-
-                Console.WriteLine(new string('*', 70));
+                ProcessClient(client);
             }
 
 
+        }
+        public static async Task ProcessClientAsync(TcpClient client)
+        {
+            using var stream = client.GetStream();//to the client take a stream  
+
+            byte[] buffer = new byte[1000000];
+            var length = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+
+            string html = $"<h1> Hello from NikiServer {DateTime.Now}</h1>" +
+                  $"<form method=post><input name=username /><input name=password />" +
+                  $"<input type=submit /></form>" + DateTime.Now;
+
+            string response = "HTTP/1.1 200 OK" + NewLine +
+                "Server: NikiServer 2022" + NewLine +
+                // "Location: https://google.com" + NewLine +
+                "Content-Type: text/html; charset=utf-8" + NewLine +
+                "Content-Lenght: " + html.Length + NewLine +
+                NewLine +
+                html +
+                NewLine;
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            await stream.WriteAsync(responseBytes);
+
+            Console.WriteLine(new string('=', 70));
+        }
+        public static void ProcessClient(TcpClient client)
+        {
+            using var stream = client.GetStream();//to the client take a stream  
+
+            byte[] buffer = new byte[1000000];
+            var length = stream.Read(buffer, 0, buffer.Length);
+
+            string requestString = Encoding.UTF8.GetString(buffer, 0, length);
+            Console.WriteLine(requestString);
+
+            string html = $"<h1> Hello from NikiServer {DateTime.Now}</h1>" +
+                   $"<form method=post><input name=username /><input name=password />" +
+                   $"<input type=submit /></form>" + DateTime.Now;
+
+            string response = "HTTP/1.1 200 OK" + NewLine +
+                "Server: NikiServer 2022" + NewLine +
+                // "Location: https://google.com" + NewLine +
+                "Content-Type: text/html; charset=utf-8" + NewLine +
+                "Content-Lenght: " + html.Length + NewLine +
+                NewLine +
+                html +
+                NewLine;
+
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseBytes);
+
+            Console.WriteLine(new string('=', 70));
         }
         public async Task ReadData()
         {
