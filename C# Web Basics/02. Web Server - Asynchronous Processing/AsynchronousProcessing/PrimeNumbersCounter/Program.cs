87a19fe72c41b75ecfe24317af461cf7c56930cc
Nikolay Pavlov <npavlovit@gmail.com>
2023-01-19 20:49:09 +0200

    add changes

--- C# Web Basics/02. Web Server - Asynchronous Processing/AsynchronousProcessing/PrimeNumbersCounter/Program.cs
+++ C# Web Basics/02. Web Server - Asynchronous Processing/AsynchronousProcessing/PrimeNumbersCounter/Program.cs
@@ -4,22 +4,66 @@ namespace PrimeNumbersCounter
 {
     internal class Program
     {
-        static object lockObj = new object();
+        private static int Count;
+        private static object lockObj = new object();
+
+
+        // One thread 
+        //  664579
+        //  00:00:09.1601008
+
+        // 4 threads
+        //  664579
+        //  00:00:03.5576738
         static void Main(string[] args)
         {
-            Task.Run(PrintPrimeCount);
-            while (true)
+            Stopwatch sw = Stopwatch.StartNew();
+
+            List<Task> tasks = new List<Task>();
+            for (int i = 1; i <= 100; i++)
             {
-                var input = Console.ReadLine();
-                Console.WriteLine(input.ToUpper());
+                var task = Task.Run(() => DownloadAsync(i));
+
+                tasks.Add(task);
             }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine(sw.Elapsed);
+
+            /*
+           Thread thread = new Thread(() => PrintPrimeCount(2, 2_500_000));
+           thread.Start(); 
+           Thread thread2 = new Thread(() => PrintPrimeCount(2_500_001, 5_000_000));
+           thread2.Start();
+           Thread thread3 = new Thread(() => PrintPrimeCount(5_000_001, 7_500_000));
+           thread3.Start();
+           Thread thread4 = new Thread(() => PrintPrimeCount(7_500_001, 10_000_000));
+           thread4.Start();
+           thread.Join();
+           thread2.Join();
+           thread3.Join();
+           thread4.Join();
+           while (true)
+           {
+               var input = Console.ReadLine();
+               Console.WriteLine(input.ToUpper());
+           }
+           */
         }
-        static void PrintPrimeCount()
+
+        static async Task DownloadAsync(int i)
+        {
+            HttpClient httpClient = new HttpClient();
+            var url = $"https://vicove.com/vic-{i}";
+            var httpResponse = await httpClient.GetAsync(url);
+            var vic = await httpResponse.Content.ReadAsStringAsync();
+            Console.WriteLine(vic.Length);
+        }
+        static void PrintPrimeCount(int start, int end)
         {
             Stopwatch sw = Stopwatch.StartNew();
-            long n = 10000000000;
-            int count = 0;
-            for (int i = 1; i <= n; i++)
+            for (int i = start; i <= end; i++)
             {
                 bool isPrime = true;
                 for (int j = 2; j <= Math.Sqrt(i); j++)
@@ -30,15 +74,17 @@ namespace PrimeNumbersCounter
                         break;
                     }
                 }
+
                 if (isPrime)
                 {
                     lock (lockObj)
                     {
-                        count++;
+                        Count++;
                     }
                 }
             }
-            Console.WriteLine(count);
+
+            Console.WriteLine(Count);
             Console.WriteLine(sw.Elapsed);
         }
     }
