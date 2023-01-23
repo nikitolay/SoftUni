1c2e2a8de3a90dfe3cc2a29dbeefebc181af617d
Nikolay Pavlov <npavlovit@gmail.com>
2023-01-23 17:16:21 +0200

    Changes after cookies and session lecture

--- C# Web Basics/01. Web Server - HTTP Protocol/HttpClientDemo/HttpClientDemo/Program.cs
+++ C# Web Basics/01. Web Server - HTTP Protocol/HttpClientDemo/HttpClientDemo/Program.cs
@@ -2,11 +2,13 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HttpClientDemo
 {
     internal class Program
     {
+        static Dictionary<string, int> SessionStorage = new Dictionary<string, int>();//string - number of session
         const string NewLine = "\r\n";
         static async Task Main(string[] args)
         {
@@ -20,6 +22,50 @@ namespace HttpClientDemo
             }
 
 
+        }
+
+        public static void ProcessClient(TcpClient client)
+        {
+            using var stream = client.GetStream();//to the client take a stream  
+
+            byte[] buffer = new byte[1000000];
+            var length = stream.Read(buffer, 0, buffer.Length);
+
+            string requestString = Encoding.UTF8.GetString(buffer, 0, length);
+            Console.WriteLine(requestString);
+
+            var sid = Guid.NewGuid().ToString();
+            var match = Regex.Match(requestString, @"sid=[^\n]*\r\n");
+            if (match.Success)
+            {
+                sid = match.Value.Substring(4);
+            }
+
+            if (!SessionStorage.ContainsKey(sid))
+            {
+                SessionStorage.Add(sid, 0);
+            }
+            SessionStorage[sid]++;//count of open given page
+
+            string html = $"<h1> Hello from NikiServer {DateTime.Now} for the {SessionStorage[sid]} time</h1>" +
+                   $"<form method=post><input name=username /><input name=password />" +
+                   $"<input type=submit /></form>" + DateTime.Now;
+
+            string response = "HTTP/1.1 200 OK" + NewLine +
+                "Server: NikiServer 2022" + NewLine +
+                // "Location: https://google.com" + NewLine +
+                "Content-Type: text/html; charset=utf-8" + NewLine +
+               $"Set-Cookie: {sid}; HttpOnly; Max-Age=" + (100 * 24 * 60 * 60) + NewLine +
+                "Content-Lenght: " + html.Length + NewLine +
+                NewLine +
+                html +
+                NewLine;
+
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseBytes);
+
+            Console.WriteLine(new string('=', 70));
         }
         public static async Task ProcessClientAsync(TcpClient client)
         {
@@ -44,42 +90,6 @@ namespace HttpClientDemo
 
             byte[] responseBytes = Encoding.UTF8.GetBytes(response);
             await stream.WriteAsync(responseBytes);
-
-            Console.WriteLine(new string('=', 70));
-        }
-        public static void ProcessClient(TcpClient client)
-        {
-            using var stream = client.GetStream();//to the client take a stream  
-
-            byte[] buffer = new byte[1000000];
-            var length = stream.Read(buffer, 0, buffer.Length);
-
-            string requestString = Encoding.UTF8.GetString(buffer, 0, length);
-            Console.WriteLine(requestString);
-
-            bool sessionSet = false;
-            if (requestString.Contains("sid"))
-            {
-                sessionSet = true;
-            }
-
-            string html = $"<h1> Hello from NikiServer {DateTime.Now}</h1>" +
-                   $"<form method=post><input name=username /><input name=password />" +
-                   $"<input type=submit /></form>" + DateTime.Now;
-
-            string response = "HTTP/1.1 200 OK" + NewLine +
-                "Server: NikiServer 2022" + NewLine +
-                // "Location: https://google.com" + NewLine +
-                "Content-Type: text/html; charset=utf-8" + NewLine +
-                (sessionSet ? ("Set-Cookie: sid=1354gsdrg232fgd; Path=/;") : string.Empty) + NewLine +
-                "Content-Lenght: " + html.Length + NewLine +
-                NewLine +
-                html +
-                NewLine;
-
-
-            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-            stream.Write(responseBytes);
 
             Console.WriteLine(new string('=', 70));
         }
